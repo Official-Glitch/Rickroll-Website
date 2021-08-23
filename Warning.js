c8106256676728b854a5ed1ff80d725c4fec1181
Official-Glitch <85814546+Official-Glitch@users.noreply.github.com>
2021-08-23 16:58:07 +1000

    Add files via upload

---
+++ Warning.js
@@ -0,0 +1,2 @@
+//I find it interesting you, the curious one, check the code. I guess it is a good thing that you're curious. But no matter how curious you are, Will you understand it?
+alert('Are you sure want to continue? This place includes malware that cannot be stopped');
